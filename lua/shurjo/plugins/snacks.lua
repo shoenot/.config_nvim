b3f7257fb1dd818557ef0b6ad932bd97b9176ece
Shurjo <mail@shurjo.me>
2026-09-07 23:48:49 +0600

    added snacks

---
+++ lua/shurjo/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    animate = { enabled = true },
+    bigfile = { enabled = true },
+    image = { enabled = true },
+    indent = { enabled = true },
+    notifier = { enabled = true },
+    scroll = { enabled = true },
+    quickfile = { enabled = true },
+    words = { enabled = true },
+  },
+}
